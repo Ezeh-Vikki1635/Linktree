031adb6522ee59e1a891eef1ae7ec9eac50a10b1
EzehVictoria <ezehvictoria86@gmail.com>
2022-11-04 00:29:51 +0100

    Remove the @ sign from twitter username

--- src/files/Profile.js
+++ src/files/Profile.js
@@ -6,7 +6,7 @@ export default function Profile() {
   return (
     <div id='profile_section' className="">
       <img id='profile__img' src={Vikki} alt=""/>
-      <h4 id='twitter' >@EzehVictoria1</h4>
+      <h4 id='twitter' >EzehVictoria1</h4>
       {false && <p id='slack'>Ezeh Victoria</p>}
     </div>
     
