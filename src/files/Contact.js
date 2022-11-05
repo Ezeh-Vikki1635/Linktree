466c43cdd79045d6cb7546349635cd8e192ebb77
EzehVictoria <ezehvictoria86@gmail.com>
2022-11-05 06:27:20 +0100

    Refactor code

--- src/files/Contact.js
+++ src/files/Contact.js
@@ -3,20 +3,21 @@ import {Route} from 'react-router-dom'
 export default function Contact() {
     return (  
         
-        <main>
-            
-        <div class="style">
-          <div>
-            <h2>
-              <span>Contact Me </span>
-            </h2>
-            <p class="para">Contact Me
-                Hi there, contact me to ask me about anything you have in mind.
-            </p>
-          </div>
-          <div class="form">
-            <form action="">
-              <div>
+    <main>
+      <div class="style">
+        <div>
+          <h2>
+            <span class="conta">Contact Me </span>
+          </h2>
+          <p class="para">
+            Hi there, contact me to ask me about anything you have in
+            mind.
+          </p>
+        </div>
+        <div class="form">
+          <form action="">
+            <div class="name">
+              <div class="label">
                 <label for="first name">First name</label>
                 <input
                   type="text"
@@ -40,38 +41,42 @@ export default function Contact() {
                   required
                 />
               </div>
-              <div>
-                <label for="email">Email</label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  placeholder="yourname@email.com"
-                  required
-                />
-              </div>
-                
-              <div>
-                <label for="message">Message</label>
-                <textarea
-                  id="message"
-                  name="message"
-                  placeholder="Send me a message and I'll reply you as soon as possible..."
-                  /*style="height: 170px"*/
-                  maxlength="150"
-                  required
-                ></textarea>
-              </div>
-              <div>
-                <p>You agree to providing your data to name who may contact you.</p>
-              </div>
-              <div>
-                <input type="submit" id="btn__submit" value="Send message"  />
-              </div>
-            </form>
-          </div>
+            </div>
+
+            <div>
+              <label for="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="yourname@email.com"
+                required
+              />
+            </div>
+
+            <div>
+              <label for="message">Message</label>
+              <textarea
+                id="message"
+                name="message"
+                placeholder="Send me a message and I'll reply you as soon as possible..."
+                style="height: 170px"
+                maxlength="150"
+                required
+              ></textarea>
+            </div>
+            <div>
+              <p>
+                You agree to providing your data to name who may contact you.
+              </p>
+            </div>
+            <div>
+              <input type="submit" id="btn__submit" value="Send message" />
+            </div>
+          </form>
         </div>
-      </main>
+      </div>
+    </main>
     );
 }
  
